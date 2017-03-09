94581dc1d55432150ea44a16290f6f8e448b79f7
Chris Skinner <c.skinner@blueyonder.co.uk>
2017-03-09 14:02:13 +0000

    Change var -> const

--- js/theme.js
+++ js/theme.js
@@ -1,6 +1,6 @@
 // Require dependencies
 // The following is a fix to prevent a console error of 'jQuery is not defined'
-var jQuery = require('jquery');
+const jQuery = require('jquery');
 window.$ = window.jQuery = jQuery;
 
 require('../vendor/bootstrap/dist/js/npm.js');
@@ -9,14 +9,14 @@ require('jquery.easing');
 jQuery.noConflict(true);
 
 // Custom JavaScript
-!function($) {
-    "use strict"; // Start of use strict
-
-    // You can use jQuery as $ inside this IIFE (Immediately Invoked Function Expression).. :)
-    // $('body').addClass('');
-	// http://gregfranko.com/blog/i-love-my-iife/
-
-}(jQuery); // End of use strict
+// !function($) {
+//    'use strict'; // Start of use strict
+//
+//    // You can use jQuery as $ inside this IIFE (Immediately Invoked Function Expression).. :)
+//    // $('body').addClass('');
+//    // http://gregfranko.com/blog/i-love-my-iife/
+//
+// }(jQuery); // End of use strict
 
 // Or you can use it here also.. :(
 // jQuery('body').addClass('');
